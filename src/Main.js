68105f9ca873b291a131e5286c63f100fdb60b83
gurkstav <gustav.dyrssen@live.se>
2019-01-23 17:02:50 +0100

    SPA incoming, error atm

---
+++ src/Main.js
@@ -0,0 +1,22 @@
+import { Component } from "react";
+
+class Main extends Component {
+    render() {
+        return (
+            <div>
+                <h1>
+                    Simple SPA
+                </h1>
+                <u1 className="header">
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/stuff">stuff</a></li>
+                    <li><a href="/contact">Contact</a></li>
+                </u1>
+                <div className="content">
+
+                </div>
+            </div>
+        );
+    }
+}
+export default Main;    
