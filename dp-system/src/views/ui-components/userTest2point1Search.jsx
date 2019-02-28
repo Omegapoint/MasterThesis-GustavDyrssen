6ff822928a24336b105a0bf5c7c834f07232f283
gurkstav <gustav.dyrssen@live.se>
2019-02-28 15:58:33 +0100

    stuff for userTest2.1Search

--- dp-system/src/views/ui-components/userTest2point1Search.jsx
+++ dp-system/src/views/ui-components/userTest2point1Search.jsx
@@ -6,9 +6,13 @@ import {
     Row,
     Card,
     CardBody,
-    CardTitle
+    CardTitle,
+    CardText,
+    CardImg,
+    CardImgOverlay
 } from 'reactstrap';
 
+import img5 from '../../assets/images/big/img5.jpg';
 import { MaterialUIPickers } from 'components/dashboard-components';
 import { TextFields, OutlinedTextFields } from 'components/dashboard-components';
 
@@ -17,6 +21,24 @@ class LayoutComponent extends React.Component {
 
     render() {
         return <div>
+            <Col xs="12" md="6">
+                {/*--------------------------------------------------------------------------------*/}
+                {/*Card-1*/}
+                {/*--------------------------------------------------------------------------------*/}
+                <Card inverse>
+                    <CardImg width="100%" src={img5} alt="Card image cap" />
+                    <CardImgOverlay>
+                        <CardBody>
+                            <CardTitle className="mb-0"><i className="mdi mdi-apps mr-2"> </i>Choose Hotel Room Booking Preferences</CardTitle>
+                        </CardBody>
+                        <CardText>There is curently a higher then normal demand of hotel rooms in the area you are searching. Beware so that the room you are looking for does not get fully booked.
+                        </CardText>
+                        <CardText>
+                            <small className="text-white">Last updated 3 mins ago</small>
+                        </CardText>
+                    </CardImgOverlay>
+                </Card>
+            </Col>
             {/*--------------------------------------------------------------------------------*/}
             {/*Start Inner Div*/}
             {/*--------------------------------------------------------------------------------*/}
@@ -24,6 +46,8 @@ class LayoutComponent extends React.Component {
             {/*Row*/}
             {/*--------------------------------------------------------------------------------*/}
             <Card>
+                <CardImg width="100%" src={img5} alt="Card image cap" />
+                <CardImgOverlay>
                 <CardBody>
                     <CardTitle className="mb-0"><i className="mdi mdi-apps mr-2"> </i>Choose Hotel Room Booking Preferences</CardTitle>
                 </CardBody>
@@ -39,11 +63,10 @@ class LayoutComponent extends React.Component {
                                 <TextFields />
                             </Col>
                         </Row>
-                        <Row>
                             <Button className="btn" color="primary" size="lg">Search for available rooms</Button>{' '}
-                        </Row>
                     </Container>
                 </CardBody>
+                </CardImgOverlay> 
             </Card>
             {/*--------------------------------------------------------------------------------*/}
             {/*Row*/}
